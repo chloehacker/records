9b7d73d66de3b4c60633e65bed65baa19fea9fb4
Chloe Hacker <35873456+chloehacker@users.noreply.github.com>
2018-03-23 07:23:19 -0400

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="records",
+    version="0.1",
+    packages=find_packages(),
+    author="Chloe Hacker",
+    description="A package for querying a REST API",
+    classifiers=["Programming Language :: Python :: 3"],
+    
